e278a5236a3b506ca0b05101767f56b9f9ee0130
Gabriel Luis da Silva <gabrielluis_21@hotmail.com>
2019-08-09 02:23:55 -0300

    Frontend development started

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const server = express();
@@ -8,6 +9,7 @@ mongoose.connect('mongodb+srv://sa:Senha_1234@cluster0-szypc.mongodb.net/omnista
     useNewUrlParser: true
 });
 
+server.use(cors());
 server.use(express.json);
 server.use(routes);
 
---
+++ Frontend/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './App.css';
+import Login from './pages/login/Login';
+
+function App() {
+  return (
+    <Login />
+  );
+}
+
+export default App;
